05950ca630b70bddcb195a468b603bcdf138a569
Jake Garrison <omonoid@uw.edu>
2016-02-18 11:57:36 -0800

    my html and js

--- backend/app/static/jakeMap.js
+++ backend/app/static/jakeMap.js
@@ -1,164 +1,118 @@
-// String format to work somewhat like pythons print formatter
-String.prototype.format = function() {
-	var i = 0,
-		args = arguments;
-	return this.replace(/{}/g, function() {
-		return typeof args[i] != 'undefined' ? args[i++] : '';
-	});
-};
-
-
-//On document creation adds click event handler to forms
-$(function() {
-	$('a#calculate').bind('click', function() {
-		$.getJSON($SCRIPT_ROOT + '/paystations_in_radius', {
-			latitude: $('input[name="latitude"]').val(),
-			longitude: $('input[name="longitude"]').val(),
-			radius: $('input[name="radius"]').val()
-		}, function(data) {
-			$.each(data.result, function(index) {
-				payStationItem = data.result[index]
-				console.log(index);
-				idNumber = payStationItem[0];
-				meterLat = payStationItem[1];
-				meterLong = payStationItem[2];
-				meterMaxOcc = payStationItem[3];
-				distance = payStationItem[4];
-				text = '<li> PayStation {} is located at ({} , {}), maximum occupancy is {} cars and is {} km away'.format(idNumber, meterLong, meterLat, meterMaxOcc, distance);
-				$("#resultList").after(text);
-
-			});
-		});
-		return false;
+// var map;
+function initMap() {
+	// Create Map
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 47.60801,
+			lng: -122.335167
+		},
+		zoom: 15,
+		disableDefaultUI: true,
+		scrollwheel: true
 	});
 
-	var map;
-	var markersList = [];
-	var infoWindowList = [];
-	//Creates map over seattle and adds click listener
-	window.initMap = function() {
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {
-				lat: 47.60801,
-				lng: -122.335167
-			},
-			zoom: 15,
-			disableDefaultUI: true,
-			scrollwheel: true
-		});
-		// map.addListener('click', function(e) {
-		// 	placeMarkerAndFindPayStations(e.latLng, map);
-		// });
+// 	// draw single line
+// 	var data = [-122.33054937746653, 47.601822730001246, -122.3308297335514, 47.602016542002914]
+// 	var coords = [
+// 		new google.maps.LatLng(data[1], data[0]),
+// 		new google.maps.LatLng(data[3], data[2])
+// 	];
+//
+// 	var polygon = new google.maps.Polygon({
+// 		clickable: false,
+// 		geodesic: true,
+// 		fillColor: "#FF0000",
+// 		fillOpacity: 0.300000,
+// 		paths: coords,
+// 		strokeColor: "#FF0000",
+// 		strokeOpacity: 1.000000,
+// 		strokeWeight: 3
+// 	});
+// 	polygon.setMap(map);
+// }
 
-		$.ajax({
-		    url: "http://127.0.0.1:5000/paystations",
-		    type: "GET",
-		    contentType: "application/json",
-		    data: JSON.stringify({'inputVar': 1}),
-		    success: function( data ) {
-		        alert( "success" + data );
-		    }
-		});
-		// $.getJSON(url, function(data) {
-		//
-		// 	//  Parse the Linestring field into an array of LatLngs
-		// 	$.each(data.data, function(index, record) {
-		// 		var mypath = new Array();
-		// 		line = JSON.parse(record.Linestring);
-		// 		//  Parse the array of LatLngs into Gmap points
-		// 		for (var i = 0; i < line.length; i++) {
-		// 			//Tokenise the coordinates
-		// 			var coords = (new String(line[i])).split(",");
-		// 			mypath.push(new google.maps.LatLng(coords[1], coords[0]));
-		// 		}
-		// 		var polyline = new google.maps.Polyline({
-		// 			path: mypath,
-		// 			strokeColor: '#ff0000',
-		// 			strokeOpacity: 1.0,
-		// 			strokeWeight: 3
-		// 		});
-		// 		polyline.setMap(map);
-		// 	});
-		//
-		// });
 
-		// //Gets data points from library and plots the markers
-		// //radius is gotten from textBox, default is 250m
-		// function placeMarkerAndFindPayStations(latLng, map) {
-		// 	clearMap();
-		// 	searchRadius = $('input[name="radius"]').val();
-		// 	if (searchRadius == 0) {
-		// 		searchRadius = .25
-		// 	}
-		// 	//Queries python API for datapoints
-		// 	$.getJSON($SCRIPT_ROOT + '/paystations_in_radius', {
-		// 		latitude: latLng.lat,
-		// 		longitude: latLng.lng,
-		// 		radius: searchRadius
-		// 	}, function(data) {
-		// 		markAndCircle(latLng, searchRadius, map);
-		// 		//Loop over each datapoint(payStation)
-		// 		$.each(data.result, function(index) {
-		// 			payStationItem = data.result[index]
-		// 			idNumber = payStationItem[0];
-		// 			meterLat = payStationItem[1];
-		// 			meterLong = payStationItem[2];
-		// 			meterMaxOcc = payStationItem[3];
-		// 			distance = payStationItem[4];
-		// 			//Adds marker and infowindow  + click listners for each payStation
-		// 			var marker = new google.maps.Marker({
-		// 				position: new google.maps.LatLng(meterLat, meterLong),
-		// 				map: map,
-		// 				icon: $SCRIPT_ROOT + '/static/parkingBlue.png'
-		// 					//have different colored parking .png files for busy/notbusy/somewhat busy
-		// 			});
-		// 			//TODO: Make a better looking Info window
-		// 			infoWindowContent = '<p>parkingMeter {} has a max capacity {} and is {} km away from destination </p>'.format(idNumber, meterMaxOcc, distance);
-		// 			var infoWindow = new google.maps.InfoWindow({
-		// 				content: infoWindowContent
-		// 			});
-		// 			marker.addListener('click', function() {
-		// 				for (var i = 0; i < infoWindowList.length; i++) {
-		// 					infoWindowList[i].close();
-		// 				}
-		// 				infoWindow.open(map, marker);
-		// 			});
-		// 			markersList.push(marker);
-		// 			infoWindowList.push(infoWindow);
-		// 		});
-		// 	});
-		// 	return false;
-		// }
+	$.getJSON("http://127.0.0.1:5000/paystations", function(result) {
+		$.each(result, function(i, line) {
+			var coords = new Array();
 
-		//Clerars the map of markers
-		function clearMap() {
-			for (var i = 0; i < markersList.length; i++) {
-				markersList[i].setMap(null);
-			}
-			infoWindowList = [];
-			markersList = [];
-		}
-		//takes a latLong object , radius , and map
-		//draws a maker and circle around point
-		function markAndCircle(searchCoord, searchRadius, map) {
-			var marker = new google.maps.Marker({
-				position: searchCoord,
-				map: map,
+			console.log(JSON.stringify(line))
+			//  [
+			// 	new google.maps.LatLng(line[0], line[1]),
+			// 	new google.maps.LatLng(line[2], line[3])
+			// ];
+			var coords = [
+			    {lat: line[0], lng: line[1]},
+			    {lat: line[2], lng: line[3]}
+			  ];
+			console.log(JSON.stringify(coords));
+
+			var polygon = new google.maps.Polygon({
+				clickable: false,
+				geodesic: true,
+				fillColor: "#FF0000",
+				fillOpacity: 0.300000,
+				paths: coords,
+				strokeColor: "#FF0000",
+				strokeOpacity: 1.000000,
+				strokeWeight: 3
 			});
-			var cityCircle = new google.maps.Circle({
-				strokeColor: '#FF0000',
-				strokeOpacity: 0.8,
-				strokeWeight: 2,
-				fillColor: '#FF0000',
-				fillOpacity: 0.35,
-				map: map,
-				center: searchCoord,
-				radius: searchRadius * 1000 //radius is in meters
-			});
-			markersList.push(marker);
-			//Adds circle into markerList so that it gets cleared at the same time
-			markersList.push(cityCircle);
-		}
-	}
+			polygon.setMap(map);
+	// 		var mypath = new Array();
+	// 		line = JSON.parse(field.Linestring);
+	// 		//Parse the array of LatLngs into Gmap points
+	// 		for (var i = 0; i < line.length; i++) {
+	// 			//Tokenise the coordinates
+	// 			var coords = (new String(line[i])).split(",");
+	// 			console.log(coords);
+			// }
+		});
+	});
+}
+	// var data = [-122.33054937746653, 47.601822730001246, -122.3308297335514, 47.602016542002914]
+	// // var mypath = new Array();
+	// console.log(data);
+	// // mypath.push(new google.maps.LatLng(data.slice(0,2), data.slice(2,4)));
+	// var mypath = [
+  //   {lat: data[0], lng: data[1]},
+  //   {lat: data[2], lng: data[3]},
+  // ];
+	// console.log(mypath);
+	// var polyline = new google.maps.Polyline({
+	// 	path: mypath,
+	// 	strokeColor: '#ff0000',
+	// 	strokeOpacity: 1.0,
+	// 	strokeWeight: 3
+	// });
+	// polyline.setMap(map);
 
-});
+	// Add lines
+	// var url = "http://127.0.0.1:5000/paystations"
+	// $.getJSON(url, function(data) {
+		//  Parse the Linestring field into an array of LatLngs
+		// $.each(data, function(index, line) {
+			// var mypath = new Array();
+			// line = JSON.parse(record);
+			// console.log(record);
+			// console.log(line);
+			// new google.maps.LatLng(line[0], line[1]));
+			//Tokanise coordinates
+			// for var i = 0; i < line.length; i++) {
+			// 	var coords = (new String(line[i])).split(",");
+			// 	mypath.push(new google.maps.LatLng(coords[1], coords[0]));
+			// }
+			// Parse the array of LatLngs into Gmap points
+			// for (var i = 0; i < line.length; i++) {
+			// 	//Tokenise the coordinates
+			// 	var coords = (new String(line[i])).split(",");
+			// 	mypath.push(new google.maps.LatLng(coords[1], coords[0]));
+			// }
+			// var polyline = new google.maps.Polyline({
+			// 	path: mypath,
+			// 	strokeColor: '#ff0000',
+			// 	strokeOpacity: 1.0,
+			// 	strokeWeight: 3
+			// });
+			// polyline.setMap(map);
+	// 	});
+	// });
